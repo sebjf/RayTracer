9d6ce4354f06e647ce4ab844497be70727666582
Sebastian Friston <sebastian.friston.12@ucl.ac.uk>
2015-06-18 17:07:35 +0100

    * modified design so triangles are read from memory, once for each set of rays (currently still only a single intersection is performed each tick) * design works, although at the moment each intersection result is overwritten by the last

--- CPUCode/RayTracerCpuCode.c
+++ CPUCode/RayTracerCpuCode.c
@@ -36,28 +36,26 @@ struct result_t
 
 int main(void)
 {
+	max_file_t *maxfile = RayTracer_init();
+	max_engine_t *engine = max_load(maxfile, "*");
+
 	int N = 16; //5 intersection tests
-
-
-	/* prepare some rays */
-
-	int rays_size = (sizeof(struct ray_t) * N);
-	struct ray_t* rays = malloc(rays_size);
-	memset(rays,0,rays_size);
-
-	for(int i = 0; i < N; i++)
-	{
-		rays[i].direction.z = 1;
-	}
-
 
 	/* prepare some triangles */
 
-	int triangles_size = (sizeof(struct triangle_t) * N);
-	struct triangle_t* triangles = malloc(triangles_size);
-	memset(triangles,0,triangles_size);
+	/* the triangles data must be a multiple of the burst width of the memory, any spare capacity should be filled with invalid triangles */
 
-	for(int i = 0; i < N; i++)
+	int total_triangles = 16;
+
+	int burst_size_in_bytes = max_get_burst_size(maxfile, NULL);
+	int triangles_per_burst = floor((float)burst_size_in_bytes)/(float)sizeof(struct triangle_t);
+	int total_bursts = ceil((float)total_triangles / (float)triangles_per_burst);
+	int triangle_size_in_bytes = total_bursts * burst_size_in_bytes;
+
+	struct triangle_t* triangles = malloc(triangle_size_in_bytes);
+	memset(triangles,0,triangle_size_in_bytes);
+
+	for(int i = 0; i < total_triangles; i++)
 	{
 		triangles[i].v0.z = 1;
 		triangles[i].v1.z = 1;
@@ -72,21 +70,54 @@ int main(void)
 
 	}
 
+	/* prepare some rays */
+
+	int rays_size = (sizeof(struct ray_t) * N);
+	struct ray_t* rays = malloc(rays_size);
+	memset(rays,0,rays_size);
+
+	for(int i = 0; i < N; i++)
+	{
+		rays[i].direction.z = 1;
+	}
+
 	/* prepare the kernel */
 
 	int results_size = (sizeof(struct result_t) * N);
 	struct result_t* results = malloc(results_size);
 	memset(results,0,results_size);
 
-	max_file_t *maxfile = RayTracer_init();
-	max_engine_t *engine = max_load(maxfile, "*");
+
+
+	/* initialise triangles */
+
+	max_actions_t* init_act = max_actions_init(maxfile, "memoryInitialisation");
+	max_set_param_uint64t(init_act, "address", 0);
+	max_set_param_uint64t(init_act, "size", triangle_size_in_bytes);
+	max_queue_input(init_act,"triangles_in",triangles,triangle_size_in_bytes);
+
+	max_run(engine, init_act);
 
 	max_actions_t* act = max_actions_init(maxfile, NULL);
 	
-	max_set_ticks(act, "RayTracerKernel", N);
-	max_queue_input(act, "triangles_in", triangles, triangles_size);
+	int triangles_per_tick = 1;
+	int rays_per_tick = 1;
+	int rays_in_set = N;
+	int triangles_in_set = total_bursts * triangles_per_burst;
+
+	int intersection_ticks = (triangles_in_set / triangles_per_tick) * (rays_in_set / rays_per_tick);
+	int memory_command_ticks = (rays_in_set / rays_per_tick);
+
+	max_set_ticks(act, "MemoryCommandGenerator", memory_command_ticks);
+	max_set_uint64t(act,"MemoryCommandGenerator","triangles_to_read_in_bursts",total_bursts);
+
+	max_ignore_lmem(act,"triangles_to_mem");
+	max_set_ticks(act, "RayTracerKernel", intersection_ticks);
+	max_set_uint64t(act,"RayTracerKernel","total_triangles",triangles_in_set);
 	max_queue_input(act, "rays_in", rays, rays_size);
 	max_queue_output(act, "results_out", results, results_size);
+
+
 
 	printf("Running on DFE...\n");
 
--- RunRules/Simulation/include/RayTracer.h
+++ RunRules/Simulation/include/RayTracer.h
@@ -10,6 +10,143 @@ extern "C" {
 
 
 /*----------------------------------------------------------------------------*/
+/*---------------------- Interface memoryInitialisation ----------------------*/
+/*----------------------------------------------------------------------------*/
+
+
+
+
+/**
+ * \brief Basic static function for the interface 'memoryInitialisation'.
+ * 
+ * \param [in] param_address Interface Parameter "address".
+ * \param [in] param_size Interface Parameter "size".
+ * \param [in] instream_triangles_in The stream should be of size param_size bytes.
+ */
+void RayTracer_memoryInitialisation(
+	int32_t param_address,
+	int32_t param_size,
+	const int32_t *instream_triangles_in);
+
+/**
+ * \brief Basic static non-blocking function for the interface 'memoryInitialisation'.
+ * 
+ * Schedule to run on an engine and return immediately.
+ * The status of the run can be checked either by ::max_wait or ::max_nowait;
+ * note that one of these *must* be called, so that associated memory can be released.
+ * 
+ * 
+ * \param [in] param_address Interface Parameter "address".
+ * \param [in] param_size Interface Parameter "size".
+ * \param [in] instream_triangles_in The stream should be of size param_size bytes.
+ * \return A handle on the execution status, or NULL in case of error.
+ */
+max_run_t *RayTracer_memoryInitialisation_nonblock(
+	int32_t param_address,
+	int32_t param_size,
+	const int32_t *instream_triangles_in);
+
+/**
+ * \brief Advanced static interface, structure for the engine interface 'memoryInitialisation'
+ * 
+ */
+typedef struct { 
+	int32_t param_address; /**<  [in] Interface Parameter "address". */
+	int32_t param_size; /**<  [in] Interface Parameter "size". */
+	const int32_t *instream_triangles_in; /**<  [in] The stream should be of size param_size bytes. */
+} RayTracer_memoryInitialisation_actions_t;
+
+/**
+ * \brief Advanced static function for the interface 'memoryInitialisation'.
+ * 
+ * \param [in] engine The engine on which the actions will be executed.
+ * \param [in,out] interface_actions Actions to be executed.
+ */
+void RayTracer_memoryInitialisation_run(
+	max_engine_t *engine,
+	RayTracer_memoryInitialisation_actions_t *interface_actions);
+
+/**
+ * \brief Advanced static non-blocking function for the interface 'memoryInitialisation'.
+ *
+ * Schedule the actions to run on the engine and return immediately.
+ * The status of the run can be checked either by ::max_wait or ::max_nowait;
+ * note that one of these *must* be called, so that associated memory can be released.
+ *
+ * 
+ * \param [in] engine The engine on which the actions will be executed.
+ * \param [in] interface_actions Actions to be executed.
+ * \return A handle on the execution status of the actions, or NULL in case of error.
+ */
+max_run_t *RayTracer_memoryInitialisation_run_nonblock(
+	max_engine_t *engine,
+	RayTracer_memoryInitialisation_actions_t *interface_actions);
+
+/**
+ * \brief Group run advanced static function for the interface 'memoryInitialisation'.
+ * 
+ * \param [in] group Group to use.
+ * \param [in,out] interface_actions Actions to run.
+ *
+ * Run the actions on the first device available in the group.
+ */
+void RayTracer_memoryInitialisation_run_group(max_group_t *group, RayTracer_memoryInitialisation_actions_t *interface_actions);
+
+/**
+ * \brief Group run advanced static non-blocking function for the interface 'memoryInitialisation'.
+ * 
+ *
+ * Schedule the actions to run on the first device available in the group and return immediately.
+ * The status of the run must be checked with ::max_wait. 
+ * Note that use of ::max_nowait is prohibited with non-blocking running on groups:
+ * see the ::max_run_group_nonblock documentation for more explanation.
+ *
+ * \param [in] group Group to use.
+ * \param [in] interface_actions Actions to run.
+ * \return A handle on the execution status of the actions, or NULL in case of error.
+ */
+max_run_t *RayTracer_memoryInitialisation_run_group_nonblock(max_group_t *group, RayTracer_memoryInitialisation_actions_t *interface_actions);
+
+/**
+ * \brief Array run advanced static function for the interface 'memoryInitialisation'.
+ * 
+ * \param [in] engarray The array of devices to use.
+ * \param [in,out] interface_actions The array of actions to run.
+ *
+ * Run the array of actions on the array of engines.  The length of interface_actions
+ * must match the size of engarray.
+ */
+void RayTracer_memoryInitialisation_run_array(max_engarray_t *engarray, RayTracer_memoryInitialisation_actions_t *interface_actions[]);
+
+/**
+ * \brief Array run advanced static non-blocking function for the interface 'memoryInitialisation'.
+ * 
+ *
+ * Schedule to run the array of actions on the array of engines, and return immediately.
+ * The length of interface_actions must match the size of engarray.
+ * The status of the run can be checked either by ::max_wait or ::max_nowait;
+ * note that one of these *must* be called, so that associated memory can be released.
+ *
+ * \param [in] engarray The array of devices to use.
+ * \param [in] interface_actions The array of actions to run.
+ * \return A handle on the execution status of the actions, or NULL in case of error.
+ */
+max_run_t *RayTracer_memoryInitialisation_run_array_nonblock(max_engarray_t *engarray, RayTracer_memoryInitialisation_actions_t *interface_actions[]);
+
+/**
+ * \brief Converts a static-interface action struct into a dynamic-interface max_actions_t struct.
+ *
+ * Note that this is an internal utility function used by other functions in the static interface.
+ *
+ * \param [in] maxfile The maxfile to use.
+ * \param [in] interface_actions The interface-specific actions to run.
+ * \return The dynamic-interface actions to run, or NULL in case of error.
+ */
+max_actions_t* RayTracer_memoryInitialisation_convert(max_file_t *maxfile, RayTracer_memoryInitialisation_actions_t *interface_actions);
+
+
+
+/*----------------------------------------------------------------------------*/
 /*---------------------------- Interface default -----------------------------*/
 /*----------------------------------------------------------------------------*/
 
@@ -19,22 +156,32 @@ extern "C" {
 /**
  * \brief Basic static function for the interface 'default'.
  * 
+ * \param [in] ticks_MemoryCommandGenerator The number of ticks for which kernel "MemoryCommandGenerator" will run.
  * \param [in] ticks_RayTracerKernel The number of ticks for which kernel "RayTracerKernel" will run.
+ * \param [in] inscalar_MemoryCommandGenerator_triangles_to_read_in_bursts Input scalar parameter "MemoryCommandGenerator.triangles_to_read_in_bursts".
+ * \param [in] inscalar_RayTracerKernel_total_triangles Input scalar parameter "RayTracerKernel.total_triangles".
  * \param [in] instream_rays_in Stream "rays_in".
  * \param [in] instream_size_rays_in The size of the stream instream_rays_in in bytes.
  * \param [in] instream_triangles_in Stream "triangles_in".
  * \param [in] instream_size_triangles_in The size of the stream instream_triangles_in in bytes.
  * \param [out] outstream_results_out Stream "results_out".
  * \param [in] outstream_size_results_out The size of the stream outstream_results_out in bytes.
+ * \param [in] lmem_address_triangles_to_mem Linear LMem control for "triangles_to_mem" stream: base address, in bytes.
+ * \param [in] lmem_arr_size_triangles_to_mem Linear LMem control for "triangles_to_mem" stream: array size, in bytes.
  */
 void RayTracer(
+	uint64_t ticks_MemoryCommandGenerator,
 	uint64_t ticks_RayTracerKernel,
+	uint64_t inscalar_MemoryCommandGenerator_triangles_to_read_in_bursts,
+	uint64_t inscalar_RayTracerKernel_total_triangles,
 	const void *instream_rays_in,
 	size_t instream_size_rays_in,
 	const void *instream_triangles_in,
 	size_t instream_size_triangles_in,
 	void *outstream_results_out,
-	size_t outstream_size_results_out);
+	size_t outstream_size_results_out,
+	size_t lmem_address_triangles_to_mem,
+	size_t lmem_arr_size_triangles_to_mem);
 
 /**
  * \brief Basic static non-blocking function for the interface 'default'.
@@ -44,36 +191,51 @@ void RayTracer(
  * note that one of these *must* be called, so that associated memory can be released.
  * 
  * 
+ * \param [in] ticks_MemoryCommandGenerator The number of ticks for which kernel "MemoryCommandGenerator" will run.
  * \param [in] ticks_RayTracerKernel The number of ticks for which kernel "RayTracerKernel" will run.
+ * \param [in] inscalar_MemoryCommandGenerator_triangles_to_read_in_bursts Input scalar parameter "MemoryCommandGenerator.triangles_to_read_in_bursts".
+ * \param [in] inscalar_RayTracerKernel_total_triangles Input scalar parameter "RayTracerKernel.total_triangles".
  * \param [in] instream_rays_in Stream "rays_in".
  * \param [in] instream_size_rays_in The size of the stream instream_rays_in in bytes.
  * \param [in] instream_triangles_in Stream "triangles_in".
  * \param [in] instream_size_triangles_in The size of the stream instream_triangles_in in bytes.
  * \param [out] outstream_results_out Stream "results_out".
  * \param [in] outstream_size_results_out The size of the stream outstream_results_out in bytes.
+ * \param [in] lmem_address_triangles_to_mem Linear LMem control for "triangles_to_mem" stream: base address, in bytes.
+ * \param [in] lmem_arr_size_triangles_to_mem Linear LMem control for "triangles_to_mem" stream: array size, in bytes.
  * \return A handle on the execution status, or NULL in case of error.
  */
 max_run_t *RayTracer_nonblock(
+	uint64_t ticks_MemoryCommandGenerator,
 	uint64_t ticks_RayTracerKernel,
+	uint64_t inscalar_MemoryCommandGenerator_triangles_to_read_in_bursts,
+	uint64_t inscalar_RayTracerKernel_total_triangles,
 	const void *instream_rays_in,
 	size_t instream_size_rays_in,
 	const void *instream_triangles_in,
 	size_t instream_size_triangles_in,
 	void *outstream_results_out,
-	size_t outstream_size_results_out);
+	size_t outstream_size_results_out,
+	size_t lmem_address_triangles_to_mem,
+	size_t lmem_arr_size_triangles_to_mem);
 
 /**
  * \brief Advanced static interface, structure for the engine interface 'default'
  * 
  */
 typedef struct { 
+	uint64_t ticks_MemoryCommandGenerator; /**<  [in] The number of ticks for which kernel "MemoryCommandGenerator" will run. */
 	uint64_t ticks_RayTracerKernel; /**<  [in] The number of ticks for which kernel "RayTracerKernel" will run. */
+	uint64_t inscalar_MemoryCommandGenerator_triangles_to_read_in_bursts; /**<  [in] Input scalar parameter "MemoryCommandGenerator.triangles_to_read_in_bursts". */
+	uint64_t inscalar_RayTracerKernel_total_triangles; /**<  [in] Input scalar parameter "RayTracerKernel.total_triangles". */
 	const void *instream_rays_in; /**<  [in] Stream "rays_in". */
 	size_t instream_size_rays_in; /**<  [in] The size of the stream instream_rays_in in bytes. */
 	const void *instream_triangles_in; /**<  [in] Stream "triangles_in". */
 	size_t instream_size_triangles_in; /**<  [in] The size of the stream instream_triangles_in in bytes. */
 	void *outstream_results_out; /**<  [out] Stream "results_out". */
 	size_t outstream_size_results_out; /**<  [in] The size of the stream outstream_results_out in bytes. */
+	size_t lmem_address_triangles_to_mem; /**<  [in] Linear LMem control for "triangles_to_mem" stream: base address, in bytes. */
+	size_t lmem_arr_size_triangles_to_mem; /**<  [in] Linear LMem control for "triangles_to_mem" stream: array size, in bytes. */
 } RayTracer_actions_t;
 
 /**
